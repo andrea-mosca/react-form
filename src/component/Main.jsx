d385f09534f3a329bdd27f1bd7567591aa81c3a5
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-26 15:35:00 +0200

    form to add an article

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,13 +1,22 @@
 import articles from "../data/articles";
+import { useState } from "react";
 
 export default function Main() {
+  const [newArticle, setNewArticle] = useState("");
+  const [articlesData, setArticlesData] = useState(articles);
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setArticlesData([...articles, newArticle]);
+    setNewArticle("");
+    console.log(articlesData);
+  };
   return (
     <main>
       <div className="container">
         <div>
           <h2>articles</h2>
           <div className="list-group">
-            {articles.map((article, index) => (
+            {articlesData.map((article, index) => (
               <a
                 href="#"
                 key={index}
@@ -17,6 +26,14 @@ export default function Main() {
               </a>
             ))}
           </div>
+          <form onSubmit={handleFormSubmit} className="my-5 d-flex gap-3">
+            <input
+              type="text"
+              value={newArticle}
+              onChange={(e) => setNewArticle(e.target.value)}
+            />
+            <button className="btn btn-primary">Send</button>
+          </form>
         </div>
       </div>
     </main>
